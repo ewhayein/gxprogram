a57e688260e8b01bfcab834137e0fca953768216
jeewon514 <jeewon514@gmail.com>
2026-05-24 15:01:18 +0900

    feat: QueryDSL 다중 필터 강좌 검색 구현

--- src/main/java/_5/gxprogram/repository/CourseRepository.java
+++ src/main/java/_5/gxprogram/repository/CourseRepository.java
@@ -1,4 +1,10 @@
 package _5.gxprogram.repository;
 
-public interface CourseRepository {
+import _5.gxprogram.domain.course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/* 강좌 Repository - QueryDSL 동적 검색 */
+@Repository
+public interface CourseRepository extends JpaRepository<course, Long>, CourseRepositoryCustom {
 }
